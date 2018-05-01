9e3f01d1435f8e56b10f254072e0415cf32d4957
Andrew Kinsey <akinsey@andrews-macbook-air-123.local>
2018-05-01 12:43:34 -0500

    teacher mode is complete

--- SunnyHillAppIdeaSolo/ViewController.swift
+++ SunnyHillAppIdeaSolo/ViewController.swift
@@ -10,34 +10,54 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var commentTextFields: [UITextField]!
     @IBOutlet var segmentedControllers: [UISegmentedControl]!
+    @IBOutlet weak var teacherModeButton: UIBarButtonItem!
     
     var password = "test"
     
-    override func viewDidLoad() {
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
     }
-
     
     @IBAction func editButtonTapped(_ sender: Any)
     {
-        let alert = UIAlertController(title: "Enter Teacher Password", message: nil, preferredStyle: .alert)
-        alert.addTextField { (passwordTextField) in
-        }
-        alert.addAction(UIAlertAction(title: "Done", style: .default, handler: { (action) in
-            let userEnteredPassword = alert.textFields![0] as UITextField
-            if userEnteredPassword.text == self.password
-            {
-                for i in 0..<self.segmentedControllers.count
-                {
-                    self.segmentedControllers[i].isEnabled = true
-                }
-                
+        if teacherModeButton.title == "Edit"
+        {
+            let alert = UIAlertController(title: "Enter Teacher Password", message: nil, preferredStyle: .alert)
+            alert.addTextField { (passwordTextField) in
             }
-        }))
-        present(alert, animated: true, completion: nil)
+            alert.addAction(UIAlertAction(title: "Done", style: .default, handler: { (action) in
+                let userEnteredPassword = alert.textFields![0] as UITextField
+                if userEnteredPassword.text == self.password
+                {
+                    for i in 0..<self.segmentedControllers.count
+                    {
+                        self.segmentedControllers[i].isEnabled = true
+                    }
+                    for i in 0..<self.commentTextFields.count
+                    {
+                        self.commentTextFields[i].isEnabled = true
+                    }
+                    self.teacherModeButton.title = "Save"
+                }
+            }))
+            present(alert, animated: true, completion: nil)
+        }
+        else if teacherModeButton.title == "Save"
+        {
+            for i in 0..<segmentedControllers.count
+            {
+                segmentedControllers[i].isEnabled = false
+            }
+            for i in 0..<commentTextFields.count
+            {
+                commentTextFields[i].isEnabled = false
+            }
+            teacherModeButton.title = "Edit"
+        }
     }
-    
 
 }
 
