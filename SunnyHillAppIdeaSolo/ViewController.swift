cb18715cfef4ccbf32f49561cb47a9ea4b5713bd
Andrew Kinsey <akinsey@andrews-macbook-air-123.local>
2018-05-03 12:14:45 -0500

    data persistence is complete

--- SunnyHillAppIdeaSolo/ViewController.swift
+++ SunnyHillAppIdeaSolo/ViewController.swift
@@ -14,11 +14,24 @@ class ViewController: UIViewController {
     @IBOutlet var segmentedControllers: [UISegmentedControl]!
     @IBOutlet weak var teacherModeButton: UIBarButtonItem!
     
+    let defaults = UserDefaults.standard
     var password = "test"
+    var scores = [Int]() {
+        didSet {
+            defaults.set(scores, forKey: "SCORE")
+        }
+    }
     
     override func viewDidLoad()
     {
         super.viewDidLoad()
+        if let savedData = defaults.object(forKey: "SCORE") as? [Int] {
+            scores = savedData
+        }
+        for i in 0..<segmentedControllers.count
+        {
+           segmentedControllers[i].selectedSegmentIndex = scores[i]
+        }
     }
     
     @IBAction func editButtonTapped(_ sender: Any)
@@ -47,15 +60,18 @@ class ViewController: UIViewController {
         }
         else if teacherModeButton.title == "Save"
         {
+            scores.removeAll()
             for i in 0..<segmentedControllers.count
             {
                 segmentedControllers[i].isEnabled = false
+                scores.append(segmentedControllers[i].selectedSegmentIndex)
             }
             for i in 0..<commentTextFields.count
             {
                 commentTextFields[i].isEnabled = false
             }
             teacherModeButton.title = "Edit"
+            
         }
     }
 
